81cc7caae3b73369937547cd7967c7933e56afb9
yasastharinda9511 <yasastharinda9511@gmail.com>
2025-12-23 15:34:28 +0530

    update vite.onfig.js file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(),
-        tailwindcss()],
+    plugins: [react(), tailwindcss()],
+    server: {
+        port: 80,  // Set the port to 80 (or another port that fits your configuration)
+        host: true,  // Allow access from external devices, necessary for cloud deployments
+        strictPort: true,  // Ensure that the specified port is used (if it's taken, Vite will error out)
+    },
 })
